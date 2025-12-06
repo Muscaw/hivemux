e8968026bf299d995c5b387da20464360793e02b
Kevin Grandjean <moi@kevingrandjean.ch>
2025-12-06 16:56:48 +0700

    Add tests

--- tests/test_model.py
+++ tests/test_model.py
@@ -29,9 +29,16 @@ class TestHoneyCombSession:
     assert honeycomb_session.to_tmux_session_name() == tmux_session_name
 
   @pytest.mark.parametrize(
-    "session_name, tmux_session_name", [("comb__dot", "comb__dot"), ("comb_project_name", "comb_project_name")]
+    "session_name, tmux_session_name",
+    [("comb__dot", "comb__dot"), ("comb_project_name", "comb_project_name")],
   )
-  def test_from_session_name(self, session_name: str, tmux_session_name: str) -> None:
+  def test_from_session_name_to_tmux_session_name(self, session_name: str, tmux_session_name: str) -> None:
     honeycomb_session = HoneyCombSession.from_session_name(session_name)
 
     assert honeycomb_session.to_tmux_session_name() == tmux_session_name
+
+  def test_from_session_fails_does_not_match_pattern(self) -> None:
+    session_name_without_prefix = "hello"
+
+    with pytest.raises(ValueError):
+      _ = HoneyCombSession.from_session_name(session_name_without_prefix)
